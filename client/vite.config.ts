ceb93320204ca5335ab677976db078b79dbc60fd
jandgdinh <jandgdinh@gmail.com>
2024-10-28 14:43:34 -0700

    added GraphQL auth.ts, server.ts, and added Schemas drectory, still need to work on resolvers and typedefs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,10 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-      '/api': {
+      // Important for MERN Setup: Here we're establishing a relationship between our two development servers.
+      // We are pointing our Vite client-side development server to proxy API requests to our server-side Node server at port 3001.
+      // Without this line, API calls would attempt to query for data from the current domain: localhost:3000
+      '/graphql': {
         target: 'http://localhost:3001',
         secure: false,
         changeOrigin: true
--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,27 @@
+// import mongoose from 'mongoose';
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
+// export default mongoose.connection;
+// Refactor the connection.ts file to use the dotenv package to load environment variables from a .env file. This will allow us to store sensitive information like the MongoDB URI in a secure location. The dotenv package will load the environment variables from the .env file into the process.env object, making them accessible throughout the application.
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const MONGODB_URI = process.env.MONGODB_URI || '';
 
-export default mongoose.connection;
+const db = async (): Promise<typeof mongoose.connection> => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log('Database connected.');
+    return mongoose.connection;
+  } catch (error) {
+    console.error('Database connection error:', error);
+    throw new Error('Database connection failed.');
+  }
+};
+
+export default db;
+
